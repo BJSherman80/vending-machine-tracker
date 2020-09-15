3fd84cc9b72f9e3f669bc152315035cfbebe5318
Brett Turing <bjsherman80@gmail.com>
2020-09-15 07:22:32 -0600

    Created machine show page spec

---
+++ spec/features/see_the_name_of_all_of_the_snacks_in_machine_with_their_price_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe 'When a user visits a vending machine show page', type: :feature do
+  scenario 'they see the snack and the price' do
+    owner = Owner.create(name: "Sam's Snacks")
+    dons  = owner.machines.create(location: "Don's Mixed Drinks")
+    soda_1 = Snack.create!(name: "Root Beer", price: 9.00)
+    soda_2 = Snack.create!(name: "Cola", price: 3.00)
+    soda_3 = Snack.create!(name: "Cream Soda", price: 6.00)
+    MachineSnack.create!(snack: soda_1, machine: dons)
+    MachineSnack.create!(snack: soda_2, machine: dons)
+    MachineSnack.create!(snack: soda_3, machine: dons)
+
+    visit "/machines/#{dons.id}"
+    save_and_open_page
+    expect(page).to have_content("#{soda_1.name}: #{soda_1.price}")
+    expect(page).to have_content("#{soda_2.name}: #{soda_2.price}")
+    expect(page).to have_content("#{soda_3.name}: #{soda_3.price}")
+  end
+end
